d05dc10565649bcefd4b6495b44a6e6de69b056a
Haena Lee <haena.lee.moon@gmail.com>
2018-09-21 10:34:45 -0700

    Changed ACF repeater for portfolio content to ACF flexible content

--- template-parts/content.php
+++ template-parts/content.php
@@ -46,27 +46,57 @@
 				get_the_title()
 			) );
 			
-			// check that ACF exists
+			// Display ACF flexible content
 			if(function_exists('get_field')){
-				if(have_rows('project_detail')){
-					while(have_rows('project_detail')){
+				if(have_rows('portfolio_content')){
+					?>
+					<div class="portfolio-content-block">	
+					<?php
+					while(have_rows('portfolio_content')){
 						the_row();
-						// store each sub-field in a variable
-						$images = get_sub_field('project_detail_images');
-						$title = get_sub_field('project_detail_name');
-						$content = get_sub_field('project_detail_description');
-						echo '<div class="project-detail-section">';
-							echo '<h3>'. $title .'</h3>';
+						if(get_row_layout() == 'text_title'){
+							$title = get_sub_field('title');
+							echo '<h4>'. $title .'</h4>';
+						} elseif(get_row_layout() == 'text_content'){
+							$content = get_sub_field('content');
 							echo '<p>'. $content .'</p>';
+						} elseif(get_row_layout() == 'images'){
+							$images = get_sub_field('image_gallery');
 							if($images){
 								foreach($images as $image){
 									echo '<img src="'. $image['url'] .'" alt="'. $image['alt'] .'">';
 								}
 							}
-						echo '</div>';
-					}
+						} // end the last elseif
+					} // end of while loop
+					?>
+					</div> <!-- end of portfolio-content-block -->
+					<?php
 				}
 			}
+
+			// ACF repeater field to add a variety of content to portfolio page
+			// check that ACF exists
+			// if(function_exists('get_field')){
+			// 	if(have_rows('project_detail')){
+			// 		while(have_rows('project_detail')){
+			// 			the_row();
+			// 			// store each sub-field in a variable
+			// 			$images = get_sub_field('project_detail_images');
+			// 			$title = get_sub_field('project_detail_name');
+			// 			$content = get_sub_field('project_detail_description');
+			// 			echo '<div class="project-detail-section">';
+			// 				echo '<h3>'. $title .'</h3>';
+			// 				echo '<p>'. $content .'</p>';
+			// 				if($images){
+			// 					foreach($images as $image){
+			// 						echo '<img src="'. $image['url'] .'" alt="'. $image['alt'] .'">';
+			// 					}
+			// 				}
+			// 			echo '</div>';
+			// 		}
+			// 	}
+			// }
 	
 			wp_link_pages( array(
 				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'haenalee' ),
