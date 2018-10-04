61b7313949504e81feafed782e697c32206934eb
Haena Lee <haena.lee.moon@gmail.com>
2018-10-04 01:22:05 -0700

    additional styles on about page

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -15,53 +15,70 @@
 	</header><!-- .entry-header -->
 
 	<div class="entry-content-about">
+		<div class="flex-content-about">
+			<div class="about-content">
+				<?php
+				the_content();
+				// display CTA text and link
+				if(function_exists('get_field')){
+					if(get_field('about_cta_text') && get_field('about_cta_link')){
+						$about_cta_text = get_field('about_cta_text');
+						$about_cta_link = get_field('about_cta_link');
+						?>
+						<p class="about-cta-button"><a href="<?php echo $about_cta_link['url']; ?>"><?php echo $about_cta_text; ?></a></p>
+						<?php
+					}
+				}
+				?>
+			</div>
+			<div class="about-picture-float"> <?php the_post_thumbnail('haenalee-small-med'); ?> </div><!-- .about-picture -->
+		</div><!-- .flex-content-about -->
 		<?php
-		the_content();
-		?>
-		<div class="about-picture">
-			<?php the_post_thumbnail('haenalee-small-med'); ?>
-		</div><!-- .about-picture -->
-		<?php
-
 		// check that ACF exists
 		if(function_exists('get_field')){
-			// parent repeater
-			if(have_rows('about_me')){
-				while(have_rows('about_me')){
-					the_row();
+			?>
+				<div class="about-picture">
+					<?php the_post_thumbnail('haenalee-small-med'); ?>
+				</div><!-- .about-picture -->
+				<?php
+				// parent repeater
+				if(have_rows('about_me')){
 					?>
-					<div class="about-container">
 					<?php
-					// store each sub-field in a variable
-						$about_title = get_sub_field('about_title');
+					while(have_rows('about_me')){
+						the_row();
 						?>
-						<h4><?php echo $about_title; ?></h4>
+						<div class="about-container">
 						<?php
-						// check for rows in child repeater
-						if(have_rows('about_list')){
-							while(have_rows('about_list')){
-								the_row();
-								?>
-								<ul>
-								<?php
-								if(get_sub_field('qualification')){
+						// store each sub-field in a variable
+							$about_title = get_sub_field('about_title');
+							?>
+							<h4><?php echo $about_title; ?></h4>
+							<?php
+							// check for rows in child repeater
+							if(have_rows('about_list')){
+								while(have_rows('about_list')){
+									the_row();
 									?>
-									<li><i class="fas fa-angle-right fa-sm"> </i> <?php echo the_sub_field('qualification'); ?></li>
+									<ul>
+									<?php
+									if(get_sub_field('qualification')){
+										?>
+										<li><i class="fas fa-angle-right fa-sm"> </i> <?php echo the_sub_field('qualification'); ?></li>
+										<?php
+									}
+									?>
+									</ul>
 									<?php
 								}
-								?>
-								</ul>
-								<?php
 							}
-						}
-					?>
-					</div><!-- .about-container -->
-					<?php
+						?>
+						</div><!-- .about-container -->
+						<?php
+					}
 				}
 			}
-		}
-
-		?>
+			?>
 	</div><!-- .entry-content -->
 
 </article><!-- #post-<?php the_ID(); ?> -->
